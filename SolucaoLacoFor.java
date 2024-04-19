8539999ba9f3acdb1ef31c636e608668c2200c7e
Caio-Charles <161898628+Caio-Charles@users.noreply.github.com>
2024-04-19 08:01:55 -0300

    Update SolucaoLacoFor.java

--- SolucaoLacoFor.java
+++ SolucaoLacoFor.java
@@ -2,23 +2,23 @@ import java.util.Scanner;
 
 public class SolucaoLacoFor {
 	public static void main(String[] args) {
-		// ENTRADA:
+		// ENTRADA E VARIÁVEIS:
 		Scanner entrada = new Scanner(System.in);
-		int valor;
+		int valor, erro = 0; // Valor: armazena a entrada do usuario ; erro: armazena a qtd de valores fora do intervalo fechado.
 
-		if ((valor = entrada.nextInt()) >= 1 && valor <= 1000) {
-			if (valor % 2 != 0) {
-				for (int i = 1; i < valor + 1; i += 2) {
-					System.out.println(i);
-				}
+		// PROCESSAMENTO COM SAÍDA:
+		for (int i = 0; i <= erro; i++) { // Garante que ao menos uma 1x a entrada será solicitada.
+			valor = entrada.nextInt();
+
+			if (valor < 1 || valor > 1000) { // Garante que a entrada seja solicitada enquanto fora de intervalo.
+				erro++;
+				continue; // Não permite que o laço subsequente seja executado, passando para próx. iteração de 'i'.
 			}
-			if (valor % 2 == 0) {
-				for (int i = 1; i < valor; i += 2) {
-					System.out.println(i);
-				}
+
+			for (int j = 1; j <= valor; j += 2) {
+				System.out.println(j);
 			}
 		}
 		entrada.close();
 	}
-
 }
